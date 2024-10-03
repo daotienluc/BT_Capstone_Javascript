4398126a1dd6b57b40e9b2595298331d0fcfdde4
MokuYagi <sangtruong02012004@gmail.com>
2024-10-03 10:33:01 +0700

    Sang commit update dang ky

---
+++ js/register.js
@@ -0,0 +1,37 @@
+document.getElementById('formRegister').addEventListener('submit', function (event) {
+    event.preventDefault();
+  
+    const fullName = document.getElementById('exampleInputUsername').value;
+    const email = document.getElementById('exampleInputEmail').value;
+    const password = document.getElementById('exampleInputPassword').value;
+    const confirmPassword = document.getElementById('exampleInputConfirmPassword').value;
+  
+    if (password !== confirmPassword) {
+      toastr.error('Passwords do not match');
+      return;
+    }
+  
+    const user = {
+      email: email,
+      password: password,
+      name: fullName,
+    };
+  
+    $.ajax({
+      url: 'https://shop.cyberlearn.vn/api/Users/signup',
+      type: 'POST',
+      contentType: 'application/json',
+      data: JSON.stringify(user),
+      success: function (response) {
+        toastr.success('Registration successful');
+        setTimeout(() => {
+            window.location.href = 'index.html'; 
+          }, 2000); 
+      },
+      error: function (error) {
+        const errorMessage = error.responseJSON?.message || 'Registration failed';
+        toastr.error(errorMessage);
+        console.error(error);
+      }
+    });
+  });
