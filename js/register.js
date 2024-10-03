f5dfc56368df717cbc0c944682e5105da616cf9f
daotienluc <lucd1177@gmail.com>
2024-10-03 11:00:04 +0700

    update css header

--- js/register.js
+++ js/register.js
@@ -1,37 +1,40 @@
-document.getElementById('formRegister').addEventListener('submit', function (event) {
+document
+  .getElementById("formRegister")
+  .addEventListener("submit", function (event) {
     event.preventDefault();
-  
-    const fullName = document.getElementById('exampleInputUsername').value;
-    const email = document.getElementById('exampleInputEmail').value;
-    const password = document.getElementById('exampleInputPassword').value;
-    const confirmPassword = document.getElementById('exampleInputConfirmPassword').value;
-  
+
+    const fullName = document.getElementById("exampleInputUsername").value;
+    const email = document.getElementById("exampleInputEmail").value;
+    const password = document.getElementById("exampleInputPassword").value;
+    const confirmPassword = document.getElementById(
+      "exampleInputConfirmPassword"
+    ).value;
+
     if (password !== confirmPassword) {
-      toastr.error('Passwords do not match');
+      toastr.error("Passwords do not match");
       return;
     }
-  
+
     const user = {
       email: email,
       password: password,
       name: fullName,
     };
-  
+
     $.ajax({
-      url: 'https://shop.cyberlearn.vn/api/Users/signup',
-      type: 'POST',
-      contentType: 'application/json',
+      url: "https://shop.cyberlearn.vn/api/Users/signup",
+      type: "POST",
+      contentType: "application/json",
       data: JSON.stringify(user),
       success: function (response) {
-        toastr.success('Registration successful');
-        setTimeout(() => {
-            window.location.href = 'index.html'; 
-          }, 2000); 
+        toastr.success("Registration successful");
+        console.log(response);
       },
       error: function (error) {
-        const errorMessage = error.responseJSON?.message || 'Registration failed';
+        const errorMessage =
+          error.responseJSON?.message || "Registration failed";
         toastr.error(errorMessage);
         console.error(error);
-      }
+      },
     });
   });
