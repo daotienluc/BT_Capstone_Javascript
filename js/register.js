c14a2ab054522858182256c3fab1fc5c6aee795f
daotienluc <lucd1177@gmail.com>
2024-10-10 14:12:10 +0700

    lực update validation trang đăng kí tài khoản

--- js/register.js
+++ js/register.js
@@ -3,15 +3,38 @@ document
   .addEventListener("submit", function (event) {
     event.preventDefault();
 
-    const fullName = document.getElementById("exampleInputUsername").value;
-    const email = document.getElementById("exampleInputEmail").value;
-    const password = document.getElementById("exampleInputPassword").value;
-    const confirmPassword = document.getElementById(
-      "exampleInputConfirmPassword"
-    ).value;
+    const fullName = document
+      .getElementById("exampleInputUsername")
+      .value.trim();
+    const email = document.getElementById("exampleInputEmail").value.trim();
+    const password = document
+      .getElementById("exampleInputPassword")
+      .value.trim();
+    const confirmPassword = document
+      .getElementById("exampleInputConfirmPassword")
+      .value.trim();
+
+    if (!/^[a-zA-Z\s]+$/.test(fullName)) {
+      toastr.error("Tên đầy đủ chỉ được chứa các chữ cái và dấu cách");
+      return;
+    }
+
+    if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)) {
+      toastr.error("Định dạng Email không hợp lệ");
+      return;
+    }
+
+    if (password.length < 8) {
+      toastr.error("Mật khẩu phải ít nhất 8 ký tự");
+      return;
+    }
+    if (!/[A-Za-z]/.test(password) || !/[0-9]/.test(password)) {
+      toastr.error("Mật khẩu phải có cả chữ và số");
+      return;
+    }
 
     if (password !== confirmPassword) {
-      toastr.error("Passwords do not match");
+      toastr.error("Mật khẩu không khớp");
       return;
     }
 
@@ -29,10 +52,13 @@ document
       success: function (response) {
         toastr.success("Registration successful");
         console.log(response);
+        // setTimeout(() => {
+        //   window.location.href = "index.html";
+        // }, 2000);
       },
       error: function (error) {
         const errorMessage =
-          error.responseJSON?.message || "Registration failed";
+          error.responseJSON?.message || "Đăng ký không thành công";
         toastr.error(errorMessage);
         console.error(error);
       },
